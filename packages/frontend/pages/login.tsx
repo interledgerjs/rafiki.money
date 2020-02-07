a1f4c79d7b28fe6cd2930799b8e01b1b4f771be7
Talon Patterson <38129947+TalPat@users.noreply.github.com>
2020-02-07 09:47:28 +0200

    Login sign up fix (#25)
    * sign up login fix
    
    * sign up login fix

--- packages/frontend/pages/login.tsx
+++ packages/frontend/pages/login.tsx
@@ -17,23 +17,25 @@ const Login: NextPage<Props> = ({login_challenge}) => {
   const formRef = useRef<HTMLFormElement>(null)
 
   const onSubmit = async data => {
-      
-    const login = await usersService.login(data.username, data.password, login_challenge).then(resp => {
-      if(resp.redirectTo) {
-        window.location.href = resp.redirectTo
-      }
-    }).catch(error => {
-      console.log(error)
-    })
-
+    if (validateEmail({ target: { value: data.username } })) {
+      const login = await usersService.login(data.username, data.password, login_challenge).then(resp => {
+        if(resp.redirectTo) {
+          window.location.href = resp.redirectTo
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+    }
   }
 
   const validateEmail = e => {
     const emailRegex = RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
     if (!emailRegex.test(e.target.value)) {
       setError("username", "invalidEmail", "Please submit a valid email address")
+      return (false)
     } else if (errors.username) {
       clearError('username')
+      return (true)
     }
   }
 
