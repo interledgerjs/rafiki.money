df39bdec63a8f9e80f9bcb93fb489614c9fd49ee
revan-wy <revan-wy@student.wethinkcode.co.za>
2020-03-02 17:57:36 +0200

    extracted all components

--- packages/frontend/pages/mandates.tsx
+++ packages/frontend/pages/mandates.tsx
@@ -70,24 +70,170 @@ const doughnutOptions = {
   }
 }
 
+class TransactionCard extends React.Component {
+  render () {
+    return (
+      <div className="flex flex-col">
+        {/* Transaction card functionally */}
+        {/* {TransactionCard("Cheque", "16.00", "2 Feb 2020", false)} */}
+        {/* Transaction card manually */}
+        <div className="my-2">
+          <Card>
+            <div className="flex justify-between">
+              <div>
+                {/* <div className="overline text-blue">Savings</div> */}
+                <div className="headline-6">1 Feb 2020</div>
+              </div>
+              <div className="self-center headline-6 text-green">
+                $ 84.00
+              </div>
+            </div>
+          </Card>
+        </div>
+        {/* {TransactionCard("Cheque", "100", "1 Feb 2020")}
+  {TransactionCard("Cheque", "50.00", "1 Feb 2020", false)} */}
+      </div>)
+  }
+}
+
+class DateBox extends React.Component {
+  render () {
+    return (
+      <div className="mt-16 body-2">
+        <div className="flex flex-row justify-between">
+          <div className="">Created</div>
+          <div className="">31-01-2020, 10:13</div>
+        </div>
+        <div className="flex flex-row justify-between">
+          <div className="">Expires</div>
+          <div className="">06-02-2020, 10:13</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class DoughnutInner extends React.Component {
+  render () {
+    return (
+      <div className="donut-inner">
+        <span className="donut-inner-used">Used<br /></span>
+        <span className="donut-inner-available">Available</span>
+      </div>
+    )
+  }
+}
+
+class DoughnutDisplay extends React.Component {
+  render () {
+    return (
+      <Doughnut
+        data={data}
+        options={doughnutOptions}
+        width={170}
+        legend={{
+          position: 'bottom',
+          display: false,
+          align: 'centre',
+          labels: {
+            fontColor: '#FF8A65',
+            usePointStyle: true
+          }
+        }}
+      />
+    )
+  }
+}
+
+class WholeDoughnut extends React.Component {
+  render () {
+    return (
+      <div className="h-64">
+        {/* <div>help</div> */}
+        <DoughnutDisplay />
+        <DoughnutInner />
+      </div>
+    )
+  }
+}
+
+class SidePanel extends React.Component {
+  render () {
+    return (
+      /* Master Div */
+      <div className="ml-8">
+        {/* Graph Card */}
+        <div className="p-4 bg-surface-elevation-1 elevation-1 rounded text-on-surface sm:max-w-full md:w-card h-full">
+          <WholeDoughnut />
+          <DateBox />
+          {/* Headline */}
+          <div className="mt-10 headline-6">Transactions</div>
+          {/* Transactions master div */}
+          <TransactionCard />
+          <TransactionCard />
+          <TransactionCard />
+        </div>
+      </div>
+    )
+  }
+}
+
+class TextInputBox extends React.Component {
+  render () {
+    return (
+      <div className='w-full h-full bg-surface'>
+        <div className='w-full max-w-xs bg-surface flex items-center'>
+          <form className='w-full max-w-xs'>
+            <div className=''>
+              <TextInput name='searchFor' label='Search' style={{ position: 'relative', height: '72px' }}></TextInput>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Selector extends React.Component {
+  render () {
+    return (
+      <div className='w-1/4 h-full bg-surface'>
+        <Select options={options} styles={customStyles} />
+      </div>
+
+    )
+  }
+}
+
+class TopRow extends React.Component {
+  render () {
+    return (
+      <div className="flex flex-row justify-between">
+        <TextInputBox />
+        <Selector />
+      </div>
+    )
+  }
+}
+
 class List extends React.Component {
-  render() {
+  render () {
     return (
       <div className="flex flex-row">
-      <div className="flex flex-col bg-surface-elevation-1 elevation-1 rounded text-on-surface">
-        <div className="flex h-10 self-end">
-          <div className="flex justify-around w-listTable">
-            <div className="w-5/12">Balance</div>
-            <div className="w-1/3">Interval</div>
-            <div className="w-1/4">Currency</div>
+        <div className="flex flex-col bg-surface-elevation-1 elevation-1 rounded text-on-surface">
+          <div className="flex h-10 self-end">
+            <div className="flex justify-around w-listTable">
+              <div className="w-5/12">Balance</div>
+              <div className="w-1/3">Interval</div>
+              <div className="w-1/4">Currency</div>
+            </div>
           </div>
+          {listline}
+          {listline}
+          {listline}
         </div>
-        {listline}
-        {listline}
-        {listline}
       </div>
-    </div>
-);
+    )
   }
 }
 
@@ -95,20 +241,7 @@ class MainView extends React.Component {
   render () {
     return (
       <div className="flex flex-col">
-        <div className="flex flex-row justify-between">
-          <div className='w-full h-full bg-surface'>
-            <div className='w-full max-w-xs bg-surface flex items-center'>
-              <form className='w-full max-w-xs'>
-                <div className=''>
-                  <TextInput name='searchFor' label='Search' style={{ position: 'relative', height: '72px' }}></TextInput>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className='w-1/4 h-full bg-surface'>
-            <Select options={options} styles={customStyles} />
-          </div>
-        </div>
+        <TopRow />
         <List />
       </div>
     )
@@ -122,66 +255,8 @@ const Account: NextPage = () => {
       <Content navigation>
         <div className="flex flex-row h-full">
           <MainView />
-          {/* Master Div */}
-          <div className="ml-8">
-            {/* Graph Card */}
-            <div className="p-4 bg-surface-elevation-1 elevation-1 rounded text-on-surface sm:max-w-full md:w-card h-full">
-              <div className="h-64">
-                {/* <div>help</div> */}
-                <Doughnut
-                  data={data}
-                  options={doughnutOptions}
-                  width={170}
-                  legend={{
-                    position: 'bottom',
-                    display: false,
-                    align: 'centre',
-                    labels: {
-                      fontColor: '#FF8A65',
-                      usePointStyle: true
-                    }
-                  }}
-                />
-                <div className="donut-inner">
-                  <span className="donut-inner-used">Used<br /></span>
-                  <span className="donut-inner-available">Available</span>
-                </div>
-              </div>
-              <div className="mt-16 body-2">
-                <div className="flex flex-row justify-between">
-                  <div className="">Created</div>
-                  <div className="">31-01-2020, 10:13</div>
-                </div>
-                <div className="flex flex-row justify-between">
-                  <div className="">Expires</div>
-                  <div className="">06-02-2020, 10:13</div>
-                </div>
-              </div>
-              {/* Headline */}
-              <div className="mt-10 headline-6">Transactions</div>
-              {/* Transactions master div */}
-              <div className="flex flex-col">
-                {/* Transaction card functionally */}
-                {/* {TransactionCard("Cheque", "16.00", "2 Feb 2020", false)} */}
-                {/* Transaction card manually */}
-                <div className="my-2">
-                  <Card>
-                    <div className="flex justify-between">
-                      <div>
-                        {/* <div className="overline text-blue">Savings</div> */}
-                        <div className="headline-6">1 Feb 2020</div>
-                      </div>
-                      <div className="self-center headline-6 text-green">
-                        $ 84.00
-                      </div>
-                    </div>
-                  </Card>
-                </div>
-                {/* {TransactionCard("Cheque", "100", "1 Feb 2020")}
-                {TransactionCard("Cheque", "50.00", "1 Feb 2020", false)} */}
-              </div>
-            </div>
-          </div>        </div>
+          <SidePanel />
+        </div>
       </Content>
     </div>
   )
