2140bf0fa101d004141ea16546b171c06c4f5e24
talpat <talonpatterson94@gmail.com>
2020-03-03 10:17:59 +0200

    invoice update

--- packages/backend/src/controllers/invoicesController.ts
+++ packages/backend/src/controllers/invoicesController.ts
@@ -14,8 +14,19 @@ export async function show (ctx: AppContext): Promise<void> {
     ctx.response.status = 404
     return
   }
+  const invoiceObj = invoice.toJSON()
 
-  ctx.body = invoice.toJSON()
+  const name = invoiceObj.subject.substring(invoiceObj.subject.indexOf('//')) + '/' + invoiceObj.id
+
+  ctx.body = {
+    name,
+    subject: invoiceObj.subject,
+    amount: invoiceObj.amount,
+    assetCode: invoiceObj.assetCode,
+    assetScale: invoiceObj.assetScale,
+    description: invoiceObj.description,
+    received: invoiceObj.received
+  }
 }
 
 export async function store (ctx: AppContext): Promise<void> {
@@ -28,7 +39,8 @@ export async function store (ctx: AppContext): Promise<void> {
       assetScale,
       assetCode,
       amount,
-      description
+      description,
+      subject
     } = ctx.request.body
 
     const invoice = await Invoice.query().insertAndFetch({
@@ -36,11 +48,25 @@ export async function store (ctx: AppContext): Promise<void> {
       assetCode,
       assetScale,
       amount: amount,
-      balance: 0n
+      balance: 0n,
+      subject,
+      received: 0n
     })
 
+    const invoiceObj = invoice.toJSON()
+
+    const name = invoiceObj.subject.substring(invoiceObj.subject.indexOf('//')) + '/' + invoiceObj.id
+
     ctx.response.status = 201
-    ctx.body = invoice.toJSON()
+    ctx.body = {
+      name,
+      subject: invoiceObj.subject,
+      amount: invoiceObj.amount,
+      assetCode: invoiceObj.assetCode,
+      assetScale: invoiceObj.assetScale,
+      description: invoiceObj.description,
+      received: invoiceObj.received
+    }
   } catch (error) {
     logger.error(error.message)
     throw error
