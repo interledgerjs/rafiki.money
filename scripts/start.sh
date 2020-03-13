a60916b1769d8e3ce1f9cb4cbe10dfe7fad1c256
revan-wy <revan-wy@student.wethinkcode.co.za>
2020-03-13 10:48:37 +0200

    added backend start and stop scripts

---
+++ scripts/start.sh
@@ -0,0 +1,9 @@
+sh setupHydra.sh
+sh setupDatabase.sh
+sh setupDatabase.sh
+cd ../packages/backend
+knex migrate:latest
+knex seed:run
+yarn install
+yarn build
+yarn start
---
+++ scripts/stop.sh
@@ -0,0 +1,2 @@
+docker container stop $(docker container ls -aq)
+docker container rm $(docker container ls -aq)
