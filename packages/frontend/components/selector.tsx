0a24e2b26580fa52dc21171da5aacd9c1d0bfbc0
jadinek <jkruger@student.wethinkcode.co.za>
2020-03-18 17:51:58 +0200

    update selector component

--- packages/frontend/components/selector.tsx
+++ packages/frontend/components/selector.tsx
@@ -1,37 +1,53 @@
-import Select from 'react-select'
+import Select from "react-select"
 
-const options = [
-  { label: 'Cheque '},
-  { label: 'Savings' }
-]
+// light mode only
+const customStyles = {
+  control: (base, state) => ({
+    ...base,
+    height: 56,
+    minHeight: 56,
+    "&:hover": {
+      borderColor: 0,
+      borderWidth: 2
+    },
+    boxShadow: state.isFocused ? "#FF8A65" : 0,
+    borderColor: state.isFocused ? "#FF8A65" : "rgba(0, 0, 0, .12)",
+    borderWidth: state.isFocused ? 2 : 1
+  }),
+  option: styles => ({
+    ...styles,
+    backgroundColor: "transparent",
+    color: "black",
+    "&:hover": {
+      backgroundColor: "#FFCCBC"
+    }
+  })
+}
 
-  // light mode only
-  const customStyles = {
-    control: (base, state) => ({
-      ...base,
-      height: 56,
-      minHeight: 56,
-      '&:hover': {
-        borderColor: 0,
-        borderWidth: 2 
-      },
-      boxShadow: state.isFocused ? '#FF8A65' : 0,
-      borderColor: state.isFocused? '#FF8A65': 'rgba(0, 0, 0, .12)',
-      borderWidth: state.isFocused? 2 : 1,
-    }), option: (styles) => ({
-       ...styles,
-       backgroundColor: 'transparent',
-       color: 'black',
-       '&:hover': {
-        backgroundColor: '#FFCCBC'
-      },
-      })
-  }
+interface Options {
+  value: number,
+  label: string
+}
 
-  const Selector = () => (
-    
-    <Select options={options}
-    styles={customStyles}/>
+type props = {
+  options: Options[],
+  onChange: (event: any) => void,
+  hint?: string
+}
+
+const Selector: React.FC<props> = props => {
+
+  return (
+    <div className="bg-surface max-w-xs undefined relative h-18 my-5">
+      <Select
+        options={props.options}
+        styles={customStyles}
+        onChange={props.onChange}
+      />
+      <p className={props.hint ? `assistiveText text-error w-full` : `invisible`}>
+        {props.hint}</p>
+    </div>
   )
+}
 
-  export default Selector
+export default Selector
