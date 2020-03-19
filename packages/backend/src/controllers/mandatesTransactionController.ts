879f3d581fbd4e6a5274cc404bcc1b72e68f1ac0
revan-wy <revan-wy@student.wethinkcode.co.za>
2020-03-19 13:18:13 +0200

    indexing mandateTransactions restricted to owner

--- packages/backend/src/controllers/mandatesTransactionController.ts
+++ packages/backend/src/controllers/mandatesTransactionController.ts
@@ -1,14 +1,37 @@
 import { AppContext } from '../app'
 import { MandateTransaction } from '../models/mandateTransaction'
+import { Mandate } from '../models/mandate'
+
+const enforce = (subject: string, mandate: Mandate): boolean => {
+  return mandate.userId.toString() === subject
+}
 
 export async function index (ctx: AppContext): Promise<void> {
   const mandateId = ctx.params.id
 
   ctx.logger.info('Getting mandate transactions', mandateId)
 
-  const mandateTransactions = await MandateTransaction.query()
+  // fetch mandate (mandate id)
+
+  const mandate: Mandate = await Mandate.query()
+    .where('id', mandateId)
+    .first()
+
+  // enforce should be run against mandate owner id
+
+  if (!enforce(ctx.state.user.sub, mandate)) {
+    ctx.status = 403
+    return
+  }
+
+  const mandateTransactions: Array<MandateTransaction> = await MandateTransaction.query()
     .where('mandateId', mandateId)
     .orderBy('createdAt', 'desc')
+
+  if (!mandateTransactions) {
+    ctx.status = 404
+    return
+  }
 
   ctx.body = mandateTransactions.map(transaction => {
     return transaction.toJSON()
