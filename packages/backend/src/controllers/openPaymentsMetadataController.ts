6162f25cd052960a5405d20922e6e7fd97f86966
jadinek <jkruger@student.wethinkcode.co.za>
2020-03-26 09:25:04 +0200

    updated supported assets

--- packages/backend/src/controllers/openPaymentsMetadataController.ts
+++ packages/backend/src/controllers/openPaymentsMetadataController.ts
@@ -2,7 +2,7 @@ import { AppContext } from '../app'
 
 const INVOICES_URL = process.env.INVOICES_URL || 'http://localhost:3001/invoices'
 const MANDATES_URL = process.env.MANDATES_URL || 'http://localhost:3001/mandates'
-const SUPPORTED_ASSETS = process.env.SUPPORTED_ASSETS || JSON.stringify(['USD'])
+const SUPPORTED_ASSETS = process.env.SUPPORTED_ASSETS || JSON.stringify([{ 'code': 'USD', 'scale': 2 }])
 const AUTHORIZATION_URL = process.env.AUTHORIZATION_URL || 'http://localhost:9000/oauth2/auth'
 const ISSUER_URL = process.env.ISSUER_URL || 'http://localhost:9000/'
 const TOKEN_URL = process.env.TOKEN_URL || 'http://localhost:9000/oauth2/token'
