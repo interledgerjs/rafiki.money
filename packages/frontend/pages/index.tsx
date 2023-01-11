c4f9f7d209e32464f1a5856bb00249713ad6ab67
Alex Lakatos <alex.lakatos.qa@gmail.com>
2023-01-11 14:51:49 +0900

    add deprecation message

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -17,18 +17,20 @@ export default function Home () {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+            We&apos;re working hard on building a new testing and development
+            environemt based on{' '}
+            <a className={styles.code} href="https://github.com/interledger/rafiki">Rafiki</a> and the{' '}
+            <a className={styles.code} href="https://docs.openpayments.guide/">Open Payments APIs</a>.
           </p>
           <div>
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://interledger.org"
               target="_blank"
               rel="noopener noreferrer"
             >
               By{' '}
               <Image
-                src="/vercel.svg"
+                src="/interledger.svg"
                 alt="Vercel Logo"
                 className={styles.vercelLogo}
                 width={100}
@@ -42,80 +44,23 @@ export default function Home () {
         <div className={styles.center}>
           <Image
             className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
+            src="/favicon.svg"
+            alt="rafiki.money Logo"
+            width={500}
+            height={400}
             priority
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
         </div>
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          <h2 className={inter.className}>
+            rafiki.money <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>
+            We&apos;ve deprecated this service, and are working on building a
+            new version of an Interledger testnet. Please check back later in
+            2023 for a preview.
+          </p>
         </div>
       </main>
     </>
